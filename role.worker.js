a331b7391a4471f2fb2eacd9952f8a3fc1e5918b
Adam Swinford <aswinford@jadam.space>
2025-06-16 14:55:18 -0400

    Consolidate creep roles into a unified 'worker' role to simplify task assignment and improve flexibility

--- role.worker.js
+++ role.worker.js
@@ -12,22 +12,22 @@ var roleWorker = {
         var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'worker' && creep.memory.currentTask == 'harvesting');
         var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'worker' && creep.memory.currentTask == 'upgrading');
         var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'worker' && creep.memory.currentTask == 'building');
-        
+
         // Check if we need harvesters (highest priority)
         if (harvesters.length < config.creepCounts.harvester) {
             return 'harvesting';
         }
-        
+
         // Check if we need upgraders (second priority)
         if (upgraders.length < config.creepCounts.upgrader) {
             return 'upgrading';
         }
-        
+
         // Check if we need builders (third priority)
         if (builders.length < config.creepCounts.builder) {
             return 'building';
         }
-        
+
         // If all roles are filled, check if there are construction sites or repairs needed
         var constructionSites = room.find(FIND_CONSTRUCTION_SITES);
         var repairTargets = room.find(FIND_STRUCTURES, {
@@ -35,15 +35,15 @@ var roleWorker = {
                 return (structure.hits/structure.hitsMax) < config.structures.repairThreshold;
             }
         });
-        
+
         if (constructionSites.length > 0 || repairTargets.length > 0) {
             return 'building';
         }
-        
+
         // Default to upgrading if no specific needs
         return 'upgrading';
     },
-    
+
     /**
      * Performs harvesting tasks (collecting energy and transferring to structures)
      * @param {Creep} creep - The creep to run the harvesting logic on
@@ -73,7 +73,7 @@ var roleWorker = {
             }
         }
     },
-    
+
     /**
      * Performs upgrading tasks (upgrading the controller)
      * @param {Creep} creep - The creep to run the upgrading logic on
@@ -88,7 +88,7 @@ var roleWorker = {
             }
         }
     },
-    
+
     /**
      * Performs building tasks (building construction sites and repairing structures)
      * @param {Creep} creep - The creep to run the building logic on
@@ -100,26 +100,26 @@ var roleWorker = {
         else {
             // Priority order for construction: spawn > extension > tower > storage > container > road > wall
             var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-            
+
             if(targets.length) {
                 // Use priority order from config
                 const priorityOrder = config.structures.buildPriority;
-                
+
                 // Sort targets by priority
                 targets.sort((a, b) => {
                     const priorityA = priorityOrder.indexOf(a.structureType);
                     const priorityB = priorityOrder.indexOf(b.structureType);
-                    
+
                     if (priorityA === priorityB) {
                         // If same priority, choose the closest one
                         const distanceA = creep.pos.getRangeTo(a);
                         const distanceB = creep.pos.getRangeTo(b);
                         return distanceA - distanceB;
                     }
-                    
+
                     return priorityA - priorityB;
                 });
-                
+
                 // Build the highest priority target
                 if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
                     utils.moveTo(creep, targets[0], {visualizePathStyle: config.pathVisuals.build});
@@ -132,7 +132,7 @@ var roleWorker = {
                         return (structure.hits/structure.hitsMax) < config.structures.repairThreshold;
                     }
                 });
-                
+
                 if(targets.length) {
                     // Sort by damage percentage (most damaged first)
                     targets.sort((a, b) => {
@@ -140,7 +140,7 @@ var roleWorker = {
                         const ratioB = b.hits / b.hitsMax;
                         return ratioA - ratioB;
                     });
-                    
+
                     if(creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
                         utils.moveTo(creep, targets[0], {visualizePathStyle: config.pathVisuals.repair});
                     }
@@ -153,7 +153,7 @@ var roleWorker = {
             }
         }
     },
-    
+
     /** @param {Creep} creep **/
     run: function(creep) {
         // Initialize currentTask if not set
@@ -161,16 +161,10 @@ var roleWorker = {
             creep.memory.currentTask = this.determineRole(creep.room);
             creep.say('🔄 ' + creep.memory.currentTask);
         }
-        
-        // Periodically check if we should switch roles (every 50 ticks)
-        if(Game.time % 50 === 0) {
-            var neededRole = this.determineRole(creep.room);
-            if(neededRole !== creep.memory.currentTask) {
-                creep.memory.currentTask = neededRole;
-                creep.say('🔄 ' + creep.memory.currentTask);
-            }
-        }
-        
+
+        // We prioritize the current task and only switch when there are no more tasks available
+        // This is handled in the individual role functions (runHarvester, runBuilder)
+
         // Run the appropriate role logic
         if(creep.memory.currentTask === 'harvesting') {
             this.runHarvester(creep);
